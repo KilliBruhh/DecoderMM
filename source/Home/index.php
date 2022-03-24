0ef034da7a2638d2003136a9768d08ea93470096
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-03-25 00:50:41 +0100

    Tailwind + folder update
    - Added tailwind
    - Made basic folders

---
+++ source/Home/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+</head>
+<body class="h-screen bg-gradient-to-b bg-black caret-whites text-white" >
+    Home Works!
+</body>
+</html>
