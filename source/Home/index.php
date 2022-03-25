08baf0cbd52713129fd55d7a244339e2e3311f04
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-03-25 01:30:43 +0100

    Home page
    - Basic shit4-

--- source/Home/index.php
+++ source/Home/index.php
@@ -6,7 +6,24 @@
   <link href="/dist/output.css" rel="stylesheet">
   <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
 </head>
-<body class="h-screen bg-gradient-to-b bg-black caret-whites text-white" >
-    Home Works!
+<!-- TODO: Add custom colors (not working) -->
+<body class="h-screen bg-black text-green-400" >
+    <div class="border-2 border-green-900 w-2/4 h-64 mx-auto mt-40">
+        <div class="border-2 border-green-900 w-2/5 mx-auto border-t-0 h-10 text-center font-bold">
+            DECODER DELUXE
+        </div>
+        <table class="mx-auto mt-6">
+            <form method="POST">
+                <tr>
+                    <td class="text-center">ENTER CODE:</td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="code" class="border-2 border-green-900 bg-transparent">
+                    </td>
+                </tr>
+            </form>
+        </table>
+    </div>
 </body>
 </html>
